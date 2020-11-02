ea9c36e1cac40586b7939fad146c7860e9b6d8e1
DESKTOP-JAEDDNL\Azhar Khan <azharkhanuos51@outlook.com>
2020-11-02 21:14:14 +0500

    UpdateEntitys

---
+++ PointOfSale.Data/PointOfSaleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointOfSale.Data
+{
+    class PointOfSaleDbContext:DbContext
+    {
+        public PointOfSaleDbContext(DbContextOptions<PointOfSaleDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
